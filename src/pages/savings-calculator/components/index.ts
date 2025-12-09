cab96e75544bd75770b4ed0714b0daafb3e51df8
mgYang53 <mg960503@gmail.com>
2025-12-10 02:38:36 +0900

    remove: 사용하지 않는 로더 컴포넌트 제거

--- src/pages/savings-calculator/components/index.ts
+++ src/pages/savings-calculator/components/index.ts
@@ -1,5 +1,4 @@
 export { default as SavingsProductsList } from './SavingsProductsList';
 export { default as RecommendedProductsList } from './RecommendedProductsList';
 export { default as CalculationResult } from './CalculationResult';
-export { default as SavingsProductsLoader } from './SavingsProductsLoader';
 export { default as SavingsCalculatorContent } from './SavingsCalculatorContent';
